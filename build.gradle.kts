102e46d83713b6dfeba16158bfb7b88f458ccab8
Jonas <burgeljonas@gmail.com>
2020-11-12 19:15:16 +0100

    added HelloWorld class + test + dependency/engine definition

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,4 +11,12 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+
+    //JUnit 5
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.1")
+}
+
+tasks.withType<Test>{
+    useJUnitPlatform()
 }
---
+++ src/test/kotlin/org/example/HelloWorldTest.kt
@@ -0,0 +1,12 @@
+package org.example
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class HelloWorldTest {
+    @Test
+    fun test(){
+        println("HelloWorldTest")
+        assertTrue(true)
+    }
+}
